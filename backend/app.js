075f13f59d0f5298cd7ff20d8178b6812af6937a
ammar <root@ubuntu2004.linuxvmimages.local>
2026-04-12 14:44:36 -0400

    Final DevOps Production Project

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const client = require("prom-client");
+
+const app = express();
+
+client.collectDefaultMetrics();
+
+app.get("/", (req, res) => {
+    res.send("Backend Running");
+});
+
+app.get("/health", (req, res) => {
+    res.status(200).send("OK");
+});
+
+// ✅ ADD THIS (VERY IMPORTANT)
+app.get("/metrics", async (req, res) => {
+    res.set("Content-Type", client.register.contentType);
+    res.end(await client.register.metrics());
+});
+
+// ✅ IMPORTANT for Docker
+app.listen(5000, "0.0.0.0", () => {
+    console.log("Server running on port 5000");
+});
